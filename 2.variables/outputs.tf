d8607b4f601987675e9bf3a18c4123e041b0cecb
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-29 19:38:06 +0530

    learning terraform variables

--- 2.variables/outputs.tf
+++ 2.variables/outputs.tf
@@ -11,5 +11,13 @@ output "second_values_in_list" {
 }
 
 output "Value_in_Map" {
-  value = var.v3["xyz"] # To access values in map just give the key and it will access its value
+  value = var.v3["xyz1"] # This will throw error as there is no key name xyz1 in the map
 }
+/*
+output "Value_in_Map1" {
+  value = var.v3["xyz"] # To access values in map just give the key and it will access its value
+}*/
+output "Value_in_Map2" {
+  value = lookup(var.v3, "xyz1", null)
+}
+#here we are using lookup where we are trying to access a key that is not present in map then print null
