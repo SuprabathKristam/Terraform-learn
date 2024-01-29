14e28570f96bc82f80d196daace429a6f9c48c9e
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-29 07:03:59 +0530

    learning terraform

--- 1.print/main.tf
+++ 1.print/main.tf
@@ -1,3 +1,3 @@
 output "one"{                 # This is a simple output block with label name as One
-  value = "Hello World"
+  value = "Hello World"       # This is a string so we gave in double quotes
 }
