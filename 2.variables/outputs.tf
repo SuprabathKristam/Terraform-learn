8e77d7cc52d2b03439948ebf796e4745d8addf47
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-29 22:21:06 +0530

    Creating instances by Terraform code

--- 2.variables/outputs.tf
+++ 2.variables/outputs.tf
@@ -20,7 +20,7 @@ output "Value_in_Map2" {
 #here we are using lookup where we are trying to access a key that is not present in map then print none
 
 output "value_in_list1" {
-  value = element(var.v2 ,4) # this will print 4th value and the index will work as 0,1,2,3,4,5,6..cycles
+  value = element(var.v2 ,4) # this will print 4th index value and the index will work as 0,1,2,3,4,5,6..cycles
     }
 
 output "fruit_details" {
---
+++ expense-v1/main.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "frontend" {       # here frontend local_label given by us
+  ami                    = ami-03265a0778a880afb  #pick this from aws ,we are picking workstation ami as we are using same for all our images
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01d072d60ba28281d"]
+
+  tags = {
+    Name = "frontend-dev"
+  }
+}
+
+resource "aws_instance" "backend" {       # here backend local_label given by us
+  ami                    = ami-03265a0778a880afb  #
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01d072d60ba28281d"]
+
+  tags = {
+    Name = "backend-dev"
+  }
+}
+
+resource "aws_instance" "mysql" {       # here mysql local_label given by us
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01d072d60ba28281d"]
+
+  tags = {
+    Name = "mysql-dev"
+  }
+}
+# Key pair is optional here we can through the documentation and under stand all the arugments
+# As per standards we are trying to main all = in one line and also separate blocks by empty line
