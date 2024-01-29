3f710cb10626fa181608d0dab78ebab01ab5dc0d
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-29 16:59:57 +0530

    learning terraform variables

--- 2.variables/outputs.tf
+++ 2.variables/outputs.tf
@@ -1,3 +1,15 @@
 output "v1" {
   value = var.v1   #accessing the variables(v1) value that is declared in variables.tf
 }
+
+output "First_values_in_list" {
+  value = var.v2[0]  # Indexing starts from 0 so it will access 0th value of v2 variable
+}
+
+output "second_values_in_list" {
+  value = var.v2[1]  # Indexing starts from 0 so it will access 1st value of v2 variable
+}
+
+output "Value_in_Map" {
+  value = var.v3["xyz"] # To access values in map just give the key and it will access its value
+}
--- 2.variables/variables.tf
+++ 2.variables/variables.tf
@@ -1,3 +1,16 @@
 variable "v1" {
-  default = "Hello world"
+  default = "Hello world"  #plain text
 }
+
+variable "v2" {
+  default = [ "Hello World", 100, true ]
+}
+#A Variables list supports different data types so we are trying string , including boolean ( yes/no)
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "Mahanya"
+  }
+}
+# A variables supports map of values and we know variables having keys and values is called as Map
