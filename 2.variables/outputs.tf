5c03e7a65ee51db22a121a50d9d260a9eda9df3e
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-29 16:39:55 +0530

    learning terraform

--- 2.variables/outputs.tf
+++ 2.variables/outputs.tf
@@ -1,3 +1,3 @@
 output "v1" {
-  value = "var.v1"     #accessing the variables(v1) value that is declared in variables.tf
+  value = var.v1   #accessing the variables(v1) value that is declared in variables.tf
 }
