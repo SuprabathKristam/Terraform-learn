b4c49c6314e5b96651a01238b2a9e32454cb5b35
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-30 20:14:35 +0530

    Learning loops

--- 5.loops-1/main.tf
+++ 5.loops-1/main.tf
@@ -1,5 +1,9 @@
 resource "null_resource" "test" {
   count = 10
 }
+# By giving count resource will become a list
 
+output "test" {
+  value = null_resource.test.*.id
+}
 #Here we are creating a null resource and asking it to run 10 times and we know that null resource does nothing but here it runs times
