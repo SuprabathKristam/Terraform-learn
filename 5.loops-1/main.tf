10b2a45c1ca01ef481abe54bda4f07523c48caad
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-31 07:23:59 +0530

    Learning loops,counts

--- 5.loops-1/main.tf
+++ 5.loops-1/main.tf
@@ -8,6 +8,7 @@ output "test" {
 }
 #Here we are creating a null resource and asking it to run 10 times and we know that null resource does nothing but here it runs times
 
+/*
 resource "aws_instance" "test" { # here frontend local_label given by us
   count                  = 3
   ami                    = "ami-0f3c7d07486cad139"
@@ -17,4 +18,18 @@ resource "aws_instance" "test" { # here frontend local_label given by us
   tags = {
     Name = "test-${count.index}" #This will provide numbers like test-1 ,test-2, test-3
   }
+}*/
+
+variable "components" {
+  default = ["frontend","backend","mysql"]
 }
+
+resource "aws_instance" "test" {
+  count                  = length(var.components)
+  ami                    = "ami-0f3c7d07486cad139"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-01d072d60ba28281d"]
+
+  tags = {
+    Name = element(var.components, count.index ) #This will pick the index values of components variables (frontend,backend,mysql)
+  }
