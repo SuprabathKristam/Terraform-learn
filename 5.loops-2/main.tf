63e7cd61663df29e021e550b142c9b651249663b
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-31 19:20:00 +0530

    learning loops on Maps

--- 5.loops-2/main.tf
+++ 5.loops-2/main.tf
@@ -23,3 +23,12 @@ resource "null_resource" "test1" {  # this is a dummy resource that absolutely d
     command = "echo ${each.key} = ${each.value}" #Here in the map one key has one value like apple=10 like that
   }
 }
+
+resource "null_resource" "test2"{  # here we are going to iterate the variables in fruits2
+  for_each = var.fruits2
+  provisioner "local-exec" {
+    command = "echo ${lookup(each.value, "name", "null")} = ${lookup(each.value, "quantity", "null")}"
+  }
+}
+
+#Here in the command above we are using lookup function to retrive the value of single element in the map on topf which we are applying each loop which will iterate the values in the variables
