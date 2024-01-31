55089bead2a09773d2fb659efabd3ff8b49c4b62
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-01-31 12:56:04 +0530

    Implementing loops to create ec2 and dns records

---
+++ expense-v3/main.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "instances" {
+  count                  = length(var.components)
+  ami                    = data.aws_ami.centos8.image_id # We are calling it from data.tf
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = element(var.components, count.index )
+  }
+}
+
+resource "aws_route53_record" "records" {
+  count   = var.components
+  zone_id = var.zone_id
+  name    = "${element(var.components,count.index)}-dev"
+  type    = "A"
+  ttl     = 30                     #time to live
+  records = [element(aws_instance.instances.*.private_ip, count.index)] #The moment you call count it will become a list
+}
+
+#Here above for records column we are fetching the details for all private ip's and iterating each through index by count function
+
+
