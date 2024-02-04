64251f8c46920f254ef1f593ae77031c625cb16e
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-02-04 20:03:36 +0530

    Implementing S3 to store terraform state file

--- 8.remote-state/main.tf
+++ 8.remote-state/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "awsbucket0910"
-    key    = "test/terraform.tfstate"
+    key    = "test/terraform.tfstate" # It will store the state file with this name in the S3 bucket
     region = "us-east-1"
   }
 }
