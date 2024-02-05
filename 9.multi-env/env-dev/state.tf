6d5f8f9248ec956b97111d24898373458b216516
Suprabath Kristam <suprabath.kristam0910@gmail.com>
2024-02-05 07:26:52 +0530

    Learning multi env using same state file

---
+++ 9.multi-env/env-dev/state.tf
@@ -0,0 +1,3 @@
+bucket = "awsbucket0910"
+key    = "test/dev/terraform.tfstate" # It will store the state file with this name in the S3 bucket
+region = "us-east-1"
---
+++ 9.multi-env/env-prod/state.tf
@@ -0,0 +1,3 @@
+bucket = "awsbucket0910"
+key    = "test/prod/terraform.tfstate" # It will store the state file with this name in the S3 bucket
+region = "us-east-1"
